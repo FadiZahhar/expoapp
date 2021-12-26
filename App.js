088cec097a3cb684a93c62e2ed063791e73b5728
Fadi Zahhar <79freelancer@gmail.com>
2021-12-26 11:15:04 +0200

    adding some demo screen with dummy content for preview and demo

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { createStore, combineReducer } from 'react-redux';
+import { Provider } from 'react-redux';
+
+import productReducer from './store/reducers/products';
+
+const rootReducer = combineReducers({
+  products: productsReducer
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  return(
+    <Provider store={store}>
+      <View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      </View>
+    </Provider>
+  )
+}
--- screens/shop/ProductOverviewScreen.js
+++ screens/shop/ProductOverviewScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FlatList } from 'react-native';
 
+
 const ProductsOverViewScreen = props => {
     return <FlatList />;
 }
